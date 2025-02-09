64c8693c229459a47824722048750efd775c1f82
Oleksandr Rybalov <xanderrybalov@gmail.com>
2025-02-09 16:56:42 +0200

    feat: added new styles for SearchInput

--- src/app/components/SearchInput.tsx
+++ src/app/components/SearchInput.tsx
@@ -3,12 +3,15 @@ interface SearchInputProps {
   setSearchText: (value: string) => void;
 }
 
-export const SearchInput = ({ searchText, setSearchText }: SearchInputProps) => {
+export const SearchInput = ({
+  searchText,
+  setSearchText,
+}: SearchInputProps) => {
   return (
     <input
       type="text"
       placeholder="Search by ID, name, email or phone"
-      className="border p-2 rounded-md w-[312px] h-[40px]"
+      className="border-2 focus:border-[#1369D9] focus:ring-4 focus:ring-[#c3d9fa] outline-none p-2 rounded-md w-[312px] h-[40px] transition-all"
       value={searchText}
       onChange={(e) => setSearchText(e.target.value)}
     />
